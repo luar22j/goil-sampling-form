25ff96cfe933023043d607f964b67568445191fd
Luar <luarjs@gmail.com>
2024-11-15 12:34:37 +0100

    fix: change i18n to only spanish

--- src/pages/Tally.tsx
+++ src/pages/Tally.tsx
@@ -1,39 +1,21 @@
 import { Link } from "react-router-dom";
-import { useTranslation } from "react-i18next";
 import "../assets/css/global.css";
-import LanguageSelector from "../components/LanguageSelector";
 
 const Tally = () => {
-  const { t, i18n } = useTranslation();
-  const currentLang = i18n.language;
-
   const getFormId = () => {
-    switch (currentLang) {
-      case "fr":
-        return "mKPpR7";
-      case "en":
-        return "mYojq5";
-      default:
-        return "31JW2l";
-    }
+    return "31JW2l";
   };
 
   return (
     <div className="global-animation">
       <div
-        className={`bg-white py-4 px-5 flex items-center md:justify-center justify-between mt-3`}
+        className={`bg-white py-4 px-5 flex items-center justify-center mt-3`}
       >
         <img src="/img/sampling-logo.png" className="w-[150px]" alt="logo" />
-        <div className="md:absolute md:right-10">
-          <LanguageSelector />
-        </div>
       </div>
 
       <div className="flex items-center">
-        <Link
-          to={`/${currentLang}`}
-          className="active-button flex items-center gap-1 m-5 mt-0"
-        >
+        <Link to="/" className="active-button flex items-center gap-1 m-5 mt-0">
           <svg
             xmlns="http://www.w3.org/2000/svg"
             fill="none"
@@ -48,7 +30,7 @@ const Tally = () => {
               d="M10.5 19.5L3 12m0 0l7.5-7.5M3 12h18"
             />
           </svg>
-          <p>{t("common.back")}</p>
+          <p>Volver</p>
         </Link>
       </div>
 
